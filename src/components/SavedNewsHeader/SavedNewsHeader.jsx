48251eaf565910fd1424c519d9fa61871a2c5683
Chad Casey <gbchadd@gmail.com>
2024-05-17 07:51:29 -0700

    added frontend / server comm logic

--- src/components/SavedNewsHeader/SavedNewsHeader.jsx
+++ src/components/SavedNewsHeader/SavedNewsHeader.jsx
@@ -1,19 +1,38 @@
 import "./SavedNewsHeader.css";
 
-const SavedNewsHeader = () => {
+const SavedNewsHeader = ({ userCardData }) => {
+  let distinctKeywords = [];
+
+  for (let card of userCardData) {
+    if (!distinctKeywords.includes(card.keyword)) {
+      distinctKeywords.push(card.keyword);
+    }
+  }
+
   return (
     <section className="saved-news-header">
       <div className="saved-news-header__container">
         <h3 className="saved-news-header__heading">Saved articles</h3>
         <p className="saved-news-header__description">
-          Elise, you have 5 saved articles
+          Elise, you have {userCardData.length || 0} saved articles
         </p>
-        <p className="saved-news-header__keywords">
-          By keywords:{" "}
-          <span className="saved-news-header__keyword">
-            Nature, Yellowstone, and 2 other
-          </span>
-        </p>
+        {distinctKeywords.length > 0 ? (
+          <p className="saved-news-header__keywords">
+            By keywords:{" "}
+            <span className="saved-news-header__keyword">
+              {distinctKeywords.length === 1 && distinctKeywords[0]}
+              {distinctKeywords.length === 2 &&
+                distinctKeywords[0] + " and " + distinctKeywords[1]}
+              {distinctKeywords.length > 2 &&
+                distinctKeywords[0] +
+                  ", " +
+                  distinctKeywords[1] +
+                  ` and ${distinctKeywords.length - 2} other`}
+            </span>
+          </p>
+        ) : (
+          ""
+        )}
       </div>
     </section>
   );
--- src/components/SignoutButton/SignoutButton.jsx
+++ src/components/SignoutButton/SignoutButton.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./SignoutButton.css";
 import { IsLoggedInContext } from "../../context/IsLoggedInContext";
