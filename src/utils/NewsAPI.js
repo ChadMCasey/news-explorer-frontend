41a45c8dd649c0b3fc5768761f22a61388f93175
Chad Casey <gbchadd@gmail.com>
2024-05-18 08:20:16 -0700

    fix proxy server address to accord w baseURL

--- src/utils/NewsAPI.js
+++ src/utils/NewsAPI.js
@@ -2,7 +2,7 @@ export default class NewsAPI {
   constructor() {
     this.apiKey = "1c1afd6c571848f1b495da9cdcd7f399";
     this.baseUrlDev = `https://newsapi.org/v2/everything?pageSize=100&sortBy=popularity&apiKey=1c1afd6c571848f1b495da9cdcd7f399&`;
-    this.baseUrlProd = `https://nomoreparties.co/news/v2/top-headlines?pageSize=100&sortBy=popularity&country=us&apiKey=1c1afd6c571848f1b495da9cdcd7f399`;
+    this.baseUrlProd = `https://nomoreparties.co/news/v2/everything?pageSize=100&sortBy=popularity&apiKey=1c1afd6c571848f1b495da9cdcd7f399&`;
     this.baseUrl =
       process.env.NODE_ENV === "production"
         ? this.baseUrlProd
